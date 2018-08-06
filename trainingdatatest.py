d419375deff480121b25af9dc8f75b615a69f51e
Alex <alex@Alexs-MacBook-Pro-3.local>
2018-08-06 11:17:03 -0400

    Initial commit

---
+++ trainingdatatest.py
@@ -0,0 +1,8 @@
+
+trainStr = ""
+
+for train_num in range(10):
+    trainStr = ("training_data_%i" %(train_num)) #did have an f there
+    # num += 1
+    path_str = ("trainingdata/%s.jpg" %(trainStr))
+    print(path_str)
